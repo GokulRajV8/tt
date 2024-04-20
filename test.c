4d75ba134611b691f70b20cd2fbb249f5b70cca0
Gokul Raj V <gokulrajv108@gmail.com>
2024-04-20 20:48:57 +0530

    Changing variable names in test

--- test.c
+++ test.c
@@ -5,53 +5,51 @@ int main() {
 
     // -- Testing Vector transform layer --
 
-    float vtl_values[8] = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0};
+    float vin_for_vtl_values[3] = {1.0, 2.0, 3.0};
+    struct Vector vin_for_vtl = vect_init_data(3, vin_for_vtl_values);
+    vect_print(&vin_for_vtl, "vin for layer");
 
+    float vtl_values[8] = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0};
     struct VectorTransformLayer vtl = vtl_init(3, 2, vtl_values);
 
-    float vin_values[3] = {1.0, 2.0, 3.0};
+    struct Vector vout_for_vtl = vect_init_bare(2);
 
-    struct Vector vin = vect_init_data(3, vin_values);
-    vect_print(&vin, "vin for layer");
-
-    struct Vector vout = vect_init_bare(2);
-
-    result = vtl_transform(&vtl, &vin, &vout);
+    result = vtl_transform(&vtl, &vin_for_vtl, &vout_for_vtl);
     if (result == 0)
-        vect_print(&vout, "vtl(vin)");
+        vect_print(&vout_for_vtl, "vtl(vin)");
     else
         fprintf(stdout, "Transformation failed");
 
     // deletion
     vtl_delete(&vtl);
-    vect_delete(&vin);
-    vect_delete(&vout);
+    vect_delete(&vin_for_vtl);
+    vect_delete(&vout_for_vtl);
 
     // -- Testing Vector transform block --
 
-    float vin1_values[3] = {1.0, 2.0, 3.0};
-
-    struct Vector vin1 = vect_init_data(3, vin1_values);
-    vect_print(&vin, "vin for block");
+    float vin_for_vtb_values[3] = {1.0, 2.0, 3.0};
+    struct Vector vin_for_vtb = vect_init_data(3, vin_for_vtb_values);
+    vect_print(&vin_for_vtb, "vin for block");
 
     uint32_t input_size[2] = {3, 2};
     float vtb_layer1_values[8] = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0};
-    float vtb_layer2_values[9] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+    float vtb_layer2_values[9] = {1.0,  3.0,  5.0,  7.0, 9.0,
+                                  11.0, 13.0, 15.0, 17.0};
     float* vtb_values[2] = {vtb_layer1_values, vtb_layer2_values};
     struct VectorTransformBlock vtb = vtb_init(2, input_size, 3, vtb_values);
 
-    struct Vector vout1 = vect_init_bare(3);
+    struct Vector vout_for_vtb = vect_init_bare(3);
 
-    result = vtb_transform(&vtb, &vin1, &vout1);
+    result = vtb_transform(&vtb, &vin_for_vtb, &vout_for_vtb);
     if (result == 0)
-        vect_print(&vout1, "vtb(vin)");
+        vect_print(&vout_for_vtb, "vtb(vin)");
     else
         fprintf(stdout, "Transformation failed");
 
     // deletion
     vtb_delete(&vtb);
-    vect_delete(&vin1);
-    vect_delete(&vout1);
+    vect_delete(&vin_for_vtb);
+    vect_delete(&vout_for_vtb);
 
     return 0;
 }
